150e79442636c0ee5ca0ee20961f426a659b73c4
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-17 12:14:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,6 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -54,6 +53,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
